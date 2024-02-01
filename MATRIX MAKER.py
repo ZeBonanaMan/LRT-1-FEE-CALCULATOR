31cf517e2f080c210155440905c29170794f913f
ZeBonanaMan <mcoygaldiano@gmail.com>
2024-02-02 00:23:18 +0800

    matrix made

---
+++ MATRIX MAKER.py
@@ -0,0 +1,4 @@
+list1 = ["roosevelt","balintawak","monumento","5th avenue","r.papa","abad santos","blumentrit","tayuman","bambang","doroteo Jose","carriedo","central terminal","united nations","pedro gil","quirino","vito cruz","gil puyat","libertad","edsa","baclaran"]
+for i in list1:
+    for j in range(len(list1)):
+        print(f"('{i}','{list1[j]}'): ,")
