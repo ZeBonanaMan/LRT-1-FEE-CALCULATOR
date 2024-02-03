4379a70906045d17c806ad1c1896165f7d0e7e47
ZeBonanaMan <mcoygaldiano@gmail.com>
2024-02-03 13:30:11 +0800

    added single journey values

--- MATRIX MAKER.py
+++ MATRIX MAKER.py
@@ -1,9 +1,12 @@
 list1 = ["roosevelt","balintawak","monumento","5th avenue","r.papa","abad santos","blumentrit","tayuman","bambang","doroteo Jose","carriedo","central terminal","united nations","pedro gil","quirino","vito cruz","gil puyat","libertad","edsa","baclaran"]
+list2 = [15, 20, 25, 30, 35]
 
-
-# for i in range(13,36):
+# for i in range(15,35,5):
 #     print(f"{i},", end=" ")
+
+
+
 
 for i in list1:
     for j in range(len(list1)):
-        print(f"('{i}','{list1[j]}'): ,")
+        print(f"('{i}','{list1[j]}'): {list2[3]} ,")
