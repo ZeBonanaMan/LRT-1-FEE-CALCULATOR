4559cdd79ad89a6282b35bd6bb2f425f1f96c425
ZeBonanaMan <mcoygaldiano@gmail.com>
2024-02-09 21:58:52 +0800

    lrt 2 time

--- MATRIX MAKER.py
+++ MATRIX MAKER.py
@@ -1,14 +1,16 @@
-list1 = ["roosevelt","balintawak","monumento","5th avenue","r.papa","abad santos","blumentrit","tayuman","bambang","doroteo Jose","carriedo","central terminal","united nations","pedro gil","quirino","vito cruz","gil puyat","libertad","edsa","baclaran"]
-list2 = [15, 20, 25, 30, 35]
+# list1 = ["roosevelt","balintawak","monumento","5th avenue","r.papa","abad santos","blumentrit","tayuman","bambang","doroteo Jose","carriedo","central terminal","united nations","pedro gil","quirino","vito cruz","gil puyat","libertad","edsa","baclaran"]
+# list2 = [15, 20, 25, 30, 35]
+
+list1 = ["Recto","Legarda","Pureza","V.Mapa","J.Ruiz","Gilmore","Betty Go-Belmonte","Araneta Center-Cubao","Anonas","Katipunan","Santolan","Marikina-Pasig","Antipolo"]
+
 
 # for i in range(15,35,5):
 #     print(f"{i},", end=" ")
 
-
+# for i in list1:
+#     print(f"'{i}',")
 
 
 # for i in list1:
 #     for j in range(len(list1)):
-#         print(f"('{i}','{list1[j]}'): {list2[3]} ,")
-
-print(list1, end="\n")
+#         print(f"('{i}','{list1[j]}'): ,")
